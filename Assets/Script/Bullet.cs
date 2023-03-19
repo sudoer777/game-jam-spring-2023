4964ea7a60fbc75a3b0086386e64108e6311699c
Ethan Reece <git@sudoer.ch>
2023-03-19 03:23:03 -0500

    Fix not shooting in direction of mouse

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -6,13 +6,13 @@ public class Bullet : MonoBehaviour
 {
     public enemyScript enemyRef;
     //private GameObject bulletSelf;
-    public float bulletSpeed;
+    //public float bulletSpeed;
     public Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
         rb2d.GetComponent<Rigidbody2D>();
-        rb2d.velocity = transform.right * bulletSpeed;
+        //rb2d.velocity = transform.right * bulletSpeed;
     }
 
     // Update is called once per frame
--- Assets/Script/shootScript.cs
+++ Assets/Script/shootScript.cs
@@ -6,13 +6,19 @@ public class shootScript : MonoBehaviour
 {
     public Transform shootingPoint;
     
-    public GameObject bulletPrefab;
+    public Rigidbody2D bulletPrefab;
 
     public GameObject chargedBullet;
     public float chargeSpeed;
     public float chargeTime;
     private bool isCharging;
+    private Rigidbody2D rb;
 
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -32,7 +38,16 @@ public class shootScript : MonoBehaviour
         }
         if(Input.GetMouseButtonDown(0))
         {
-            Instantiate(bulletPrefab, shootingPoint.position, shootingPoint.rotation);
+            Rigidbody2D bullet = Instantiate(bulletPrefab, shootingPoint.position, shootingPoint.rotation);
+
+            if (rb.transform.localScale.x < 0.0f)
+            {
+                bullet.velocity = Vector2.right * -10f;
+            }
+            else
+            {
+                bullet.velocity = Vector2.right * 10f;
+            }
             //bulletPrefab.transform.localScale = new Vector3(transform.localScale.x, 0, 0);
             chargeTime = 0;
         }
