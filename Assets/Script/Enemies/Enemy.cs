2602eda26d777c38d940b650aa1447b9dff33444
Ethan Reece <git@sudoer.ch>
2023-03-19 04:21:41 -0500

    Add enemy explosion and bullet damage

--- Assets/Script/Enemies/Enemy.cs
+++ Assets/Script/Enemies/Enemy.cs
@@ -6,67 +6,73 @@ namespace Script.Enemies
 {
     public abstract class Enemy : MonoBehaviour
     {
-            private float shootTimer;
-            private Vector3 startPosition;
-            private Vector3 velocity;
-            private Rigidbody2D rb;
-            private SpriteRenderer sRenderer;
-        
-            public float movementRadius;
-            public float movementSpeed;
-            public float shootDelay;
-            public GameObject projectile;
-            public float HP;
-        
-            private const float maxHP = 100;
-        
-            // Start is called before the first frame update
-            void Start()
+        private float shootTimer;
+        private Vector3 startPosition;
+        private Vector3 velocity;
+        private Rigidbody2D rb;
+        private SpriteRenderer sRenderer;
+    
+        public float movementRadius;
+        public float movementSpeed;
+        public float shootDelay;
+        public GameObject projectile;
+        public float HP;
+        public GameObject explosion;
+    
+        private const float maxHP = 100;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            HP = maxHP;
+            startPosition = transform.position;
+            shootTimer = 0;
+            velocity = (Vector3.up + Vector3.right).normalized * movementSpeed;
+            rb = gameObject.GetComponent<Rigidbody2D>();
+            sRenderer = gameObject.GetComponent<SpriteRenderer>();
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            //destroy enemy if health falls below 0
+            if (HP <= 0)
             {
-                HP = maxHP;
-                startPosition = transform.position;
+                Instantiate(explosion, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+    
+            //shoot projectile
+            if (shootTimer >= shootDelay)
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
                 shootTimer = 0;
-                velocity = (Vector3.up + Vector3.right).normalized * movementSpeed;
-                rb = gameObject.GetComponent<Rigidbody2D>();
-                sRenderer = gameObject.GetComponent<SpriteRenderer>();
             }
-        
-            // Update is called once per frame
-            void Update()
+            shootTimer += Time.deltaTime;
+        }
+    
+        private void FixedUpdate()
+        {
+            //change horizontal movement
+            if (transform.position.x < startPosition.x && velocity.x < 0 || transform.position.x > startPosition.x + movementRadius && velocity.x > 0)
             {
-                //destroy enemy if health falls below 0
-                if (HP <= 0)
-                {
-                    Destroy(gameObject);
-                }
-        
-                //shoot projectile
-                if (shootTimer >= shootDelay)
-                {
-                    Instantiate(projectile, transform.position, Quaternion.identity);
-                    shootTimer = 0;
-                }
-                shootTimer += Time.deltaTime;
+                velocity.x *= -1;
             }
-        
-            private void FixedUpdate()
+    
+            //change vertical movement
+            if (transform.position.y < startPosition.y - (movementRadius/2) && velocity.y < 0 || transform.position.y > startPosition.y + (movementRadius/2) && velocity.y > 0)
             {
-                //change horizontal movement
-                if (transform.position.x < startPosition.x && velocity.x < 0 || transform.position.x > startPosition.x + movementRadius && velocity.x > 0)
-                {
-                    velocity.x *= -1;
-                }
-        
-                //change vertical movement
-                if (transform.position.y < startPosition.y - (movementRadius/2) && velocity.y < 0 || transform.position.y > startPosition.y + (movementRadius/2) && velocity.y > 0)
-                {
-                    velocity.y *= -1;
-                }
-        
-                //move
-                rb.MovePosition(transform.position + velocity);
+                velocity.y *= -1;
             }
+    
+            //move
+            rb.MovePosition(transform.position + velocity);
+        }
 
+        public void DealDamage(float damage)
+        {
+            HP -= damage;
+        }
     }
 }
 
